49c9ed083228ff37a03ddca824ed889163359028
Denis <denismishchenko13@gmail.com>
2017-08-13 11:56:06 +0300

    Добавил проект

---
+++ Lesson_2_HW/Lesson_2_HW/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Lesson_2_HW
+//
+//  Created by Denis on 13.08.17.
+//  Copyright © 2017 Denis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
