5ac86184b231d32de4601ebd6e740ad769b9d52c
Denis <denismishchenko13@gmail.com>
2017-08-15 18:45:23 +0300

    Использование операторов

--- Lesson_2_HW/Lesson_2_HW/main.m
+++ Lesson_2_HW/Lesson_2_HW/main.m
@@ -8,10 +8,41 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool
+    {
+        CGFloat priceOfLatte=20.5;
+        CGFloat priceOfCappuccino=18.5;
+        CGFloat priceOfEspresso=17.5;
+        CGFloat priceOfAmericano=15.5;
+        
+        NSInteger currentClientsCount = 15;
+        NSInteger oneHourAgoClientsCount = 23;
+        
+        NSInteger latteSoldCount= 4;
+        NSInteger cappuccinoSoldCount = 5;
+        NSInteger espressoSoldCount = 3;
+        NSInteger americanoSoldCount = 7;
+        
+        cappuccinoSoldCount =+3;
+        americanoSoldCount =+4;
+        currentClientsCount =-3;
+        
+        BOOL even;
+        
+        if(currentClientsCount%2==0)
+       {
+           even = true;
+
+       }
+        else
+      {
+          even = false;
+      }
+        
+        NSlog(@"Всего продано количество порций: %lu", (latteSoldCount + cappuccinoSoldCount + espressoSoldCount + americanoSoldCount));
+        
+        Nslog(@"сумма денег, вырученная за все проданные порции кофе: %f", (priceOfLatte * latteSoldCount + priceOfCappuccino * cappuccinoSoldCount + priceOfEspresso * espressoSoldCount + priceOfAmericano * americanoSoldCount));
     return 0;
 }
